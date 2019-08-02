677227ec8d261a11957a6e9bf1b48b8cc75ec16a
dianaglezlag@gmail.com <dianaglezlag@gmail.com>
2019-08-02 16:11:33 -0500

    menu y muro

---
+++ main.js
@@ -0,0 +1,10 @@
+//post para la red social
+/*
+const app = firebase.app();
+const db = firebase.firestore();
+const post = db.collection ('post').doc('wallpost');
+post.get()
+.then (doc => {
+  const data = doc.data();
+  document.write(data.news + )
+})*/
