847be8c97d419b718b9c3746cd19dfd27f456550
dianaglezlag@gmail.com <dianaglezlag@gmail.com>
2019-10-21 15:17:54 -0500

    actualizado

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", event => {
 
     const db = firebase.firestore();
 
-    const post = db.collection('post');
+    let post = db.collection('post');
 
     //var storage = customApp.storage();
     let containerPost = document.getElementById("containerPost");
@@ -15,19 +15,30 @@ document.addEventListener("DOMContentLoaded", event => {
             querySnapshot.forEach((doc) => {
                 console.log(`${doc.id} => ${doc.data()}`);
                 const userPostData = doc.data();
+                console.log("Holi "+`${userPostData.likes}`);
                 const userPost =
-                    `<div class="card"> +
-                                <img src="${userPostData.profilePicUrl}"> +
-                                <div class="container"> +
-                                    <h4>Name: ${userPostData.name}</h4> +
-                                    <p>Status: ${userPostData.status}</p> +
-                                    <p>Time: ${userPostData.timestamp}</p> +
-                                    <p>Text: ${userPostData.text}</p> +
-                                    <p>Likes: ${userPostData.likes}</p> +
-                                </div> +
+                    `<div class="card"> 
+                                <img src="${userPostData.profilePicUrl}"> 
+                                <div class="container"> 
+                                    <h4>Name: ${userPostData.name}</h4> 
+                                    <p>Status: ${userPostData.status}</p> 
+                                    <p>Time: ${userPostData.day} ${userPostData.hour}</p>
+                                    <p>Text: ${userPostData.text}</p> 
+                                    <p>Likes: ${userPostData.likes}</p> 
+                                    <button onclick="contadorLikes('${doc.id}')">Laik</button>
+                                    <button class = "editortexto" id ="${doc.id}">Editar</button>
+                                    <button class = "elimarPost" id ="${doc.id}">Eliminar</button>
+                                </div> 
                             </div>;`
                 containerPost.innerHTML += userPost
             });
+
+            let eliminarPost = document.querySelectorAll(".eliminarPost");
+            for(let i = 0; i < eliminarPost.length; i++) {
+                eliminarPost [i].addEventListener("click", () => {
+                    const postAidi = event.target.id;
+                })
+            }
         });
     });
 
@@ -45,7 +56,10 @@ document.addEventListener("DOMContentLoaded", event => {
             profilePicUrl: getProfilePicUrl(),
             status: statusPost,
             text: textPost.value + " " + indications.value + " " + zone,
-            timestamp: getCurrentDate(),
+            //timestamp: getCurrentDate(),
+            date: new Date(),
+            day: new Date().toLocaleDateString(),
+            hour: new Date().toLocaleTimeString(),
             likes: "0"
         }).then(function (docRef) {
             console.log("Document written with ID: ", docRef.id);
@@ -79,30 +93,142 @@ function getUserEmail() {
     return firebase.auth().currentUser.email;
 }
 
-function getCurrentDate() {
-    const d = new Date,
-        dformat = [d.getMonth() + 1,
-        d.getDate(),
-        d.getFullYear()].join('/') + ' ' +
-            [d.getHours(),
-            d.getMinutes(),
-            d.getSeconds()].join(':');
-    return d;
+function getUserLikes() {
+    return firebase.auth().currentUser.likes;
 }
 
+// function getCurrentDate() {
+//     const d = new Date,
+//         dformat = [d.getMonth() + 1,
+//         d.getDate(),
+//         d.getFullYear()].join('/') + ' ' +
+//             [d.getHours(),
+//             d.getMinutes(),
+//             d.getSeconds()].join(':');
+//     return d;
+// }
+
+const db = firebase.firestore();
+
+const increment = firebase.firestore.FieldValue.increment (1);
+const decrement = firebase.firestore.FieldValue.increment(-1);
+
+const storyRef = db.collection('laiks').doc.id;
+
+storyRef.update({ count: incremet });
+storyRef.update({ count: decrement });
 
 
-/*
-//likes
-function likes() {
-    if (document.getElementById("iLike").checked) {
-        total = total + 1;
-        document.getElementById("totalLikes").innerHTML = total;
-    };
-};
+// let like = document.getElementById("Laiks");
+// like.addEventListener('click', () => {
+//     post.update({
+//         likes: "3"
+//     }).then(function (docRef) {
+//         console.log("Document written with ID: ", docRef.id);
+//     }).catch(function (error) {
+//         console.error("Error adding document: ", error);
+//     });
+// });
 
-// Signs-out of Friendly Chat.
-function signOut() {
-  // Sign out of Firebase.
-  firebase.auth().signOut();
-};*/
+
+// //Funcion para agregar los Likes
+// function contadorLikes(id){
+//          let postdb = db.collection('post').doc(id);
+// // Atomically increment the population of the city by 1.
+//      postdb.update({
+//      likes: firebase.firestore.FieldValue.increment(1)
+//  }
+//  };
+
+//  const db = firebase.firestore();
+//  const increment = firebase.firestore.FieldValue.increment(1);
+ 
+//  // Document reference
+//  const storyRef = db.collection('laiks').doc.id);
+ 
+
+
+
+// likesBtn.addEventListener('click',() => {
+//    let 
+
+// Vamoacalar la funcion 
+// function addLike(){
+//     db.collection.child().child("likes")
+// } 
+
+
+
+
+
+
+
+
+// //CONTADOR DE  likes dianaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+// // counters/${ID}
+// {
+//     "num_shards": NUM_SHARDS,
+//     "shards": [subcollection]
+//   }
+  
+//   // counters/${ID}/shards/${NUM}
+//   {
+//     "count": 123
+//   }
+
+//   function createCounter(ref, num_shards) {
+//     var batch = db.batch();
+
+//     // Initialize the counter document
+//     batch.set(ref, { num_shards: num_shards });
+
+//     // Initialize each shard with count=0
+//     for (let i = 0; i < num_shards; i++) {
+//         let shardRef = ref.collection('shards').doc(i.toString());
+//         batch.set(shardRef, { count: 0 });
+//     }
+
+//     // Commit the write batch
+//     return batch.commit();
+// }
+
+// function incrementCounter(db, ref, num_shards) {
+//     // Select a shard of the counter at random
+//     const shard_id = Math.floor(Math.random() * num_shards).toString();
+//     const shard_ref = ref.collection('shards').doc(shard_id);
+
+//     // Update count
+//     return shard_ref.update("count", firebase.firestore.FieldValue.increment(1));
+// }
+
+// function getCount(ref) {
+//     // Sum the count of each shard in the subcollection
+//     return ref.collection('shards').get().then(snapshot => {
+//         let total_count = 0;
+//         snapshot.forEach(doc => {
+//             total_count += doc.data().count;
+//         });
+
+//         return total_count;
+//     });
+// }
+
+
+
+// //dianaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa//
+
+
+
+// function likes() {
+//     if (document.getElementById("iLike").checked) {
+//         total = total + 1;
+//         document.getElementById("totalLikes").innerHTML = total;
+//     };
+// };
+
+// // Signs-out of Friendly Chat.
+// // function signOut() {
+// //   // Sign out of Firebase.
+// //   firebase.auth().signOut();
+// // };*/
